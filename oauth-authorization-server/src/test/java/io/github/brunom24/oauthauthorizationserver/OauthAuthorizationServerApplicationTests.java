f324e3faa9f5269f600d25c025b6270f0c169a63
BrunoM24 <brunocostamatos.24@gmail.com>
2020-08-11 15:55:58 +0100

    Authorization Server Created

---
+++ oauth-authorization-server/src/test/java/io/github/brunom24/oauthauthorizationserver/OauthAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.brunom24.oauthauthorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthAuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
